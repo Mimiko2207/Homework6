c228c2329f51d1c7a55c28ef3591e472087c42cc
Mimiko2207 <loparev.m@mail.ru>
2025-07-06 15:27:07 +0700

    ДЗ - 6

---
+++ src/Main.java
@@ -0,0 +1,68 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Задача 1");
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Интеграция числа " + i);
+        }
+
+        System.out.println("Задача 2");
+
+        for (int i = 10; i >= 1; i --) {
+            System.out.println("Интеграция числа " + i);
+        }
+
+        System.out.println("Задача 3");
+
+        for (int i = 0; i <= 17; i += 2) {
+            System.out.println("Интеграция числа " + i);
+        }
+
+        System.out.println("Задача 4");
+
+        for (int i = 10; i >= -10; i --){
+            System.out.println("Интеграция числа " + i);
+        }
+
+        System.out.println("Задача 5");
+        for (int i = 1904; i <= 2096; i += 4){
+            System.out.println(i + " год является високосным");
+        }
+
+        System.out.println("Задача 6");
+        for (int i = 0; i<= 98; i += 7){
+            System.out.println("Интеграция числа " + i);
+        }
+
+        System.out.println("Задача 7");
+        for (int i = 1; i <= 512; i *= 2){
+            System.out.println("Интеграция числа " + i);
+        }
+
+        System.out.println("Задача 8");
+        int salary = 29000;
+        int total = 0;
+        for (int i = 0; i <= 12; i++){
+            total = total + salary;
+            System.out.println("Месяц " + i + " ,сумма накоплений равна " + total + " рублей");
+        }
+
+        System.out.println("Задача 9");
+        int money = 29000;
+        int only = 0;
+        for (int i = 0; i <= 12; i++){
+            only = only + only/100;
+            only = only + money;
+            System.out.println("Месяц " + i + " ,сумма накоплений равна " + only + " рублей");
+        }
+
+        System.out.println("Задача 10");
+        for (int i = 1; i <= 10; i ++){
+            System.out.println("2 * " + i + "=" + i * 2);
+        }
+    }
+}
